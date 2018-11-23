9ce20d18139bc103d4e649cee041bd4e8370df2c
cypherix93 <bikram.ghosh93@gmail.com>
2018-11-22 23:04:42 -0500

    Release

--- dist/di/Container.d.ts
+++
@@ -1,6 +0,0 @@
-import 'reflect-metadata';
-import Bottle from 'bottlejs';
-declare const bottle: Bottle;
-export declare function Service(): PropertyDecorator;
-export default bottle;
-//# sourceMappingURL=Container.d.ts.map
