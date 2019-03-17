e65867d7c1b8788dc9e67546adf0f3333b82bbf0
cypherix93 <bikram.ghosh93@gmail.com>
2019-03-16 20:53:20 -0400

    Multi image upload now possible

--- src/api/endpoints/images.ts
+++ src/api/endpoints/images.ts
@@ -6,4 +6,10 @@ export default class extends ApiEndpoint {
 
         return response.data;
     }
+
+    async postMultiple(data: FormData): Promise<ImageResponse[]> {
+        const response = await this.adapter.post<ImageResponse[]>('/images/multiple', data);
+
+        return response.data;
+    }
 }
